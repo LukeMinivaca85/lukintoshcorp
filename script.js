d229ddbbdcc2c3fd90b68e7bf3cf0312367e20f2
LukeMinivaca85 <rockgsamesp@gmail.com>
2025-04-24 20:20:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Ainda sem interações, mas pronto pra futuras animações
-console.log("Site Lukintosh carregado com sucesso!");
+// Adicionar interatividade se necessário, por exemplo, um efeito de rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
